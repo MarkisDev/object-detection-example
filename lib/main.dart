ee07604101a2a07e4cf69fb63af5eb85c010dda0
Rijuth Menon <rijuthm@gmail.com>
2023-04-21 17:38:24 +0530

    removed example from title

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Object Detection Example',
+        title: 'Object Detection',
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
